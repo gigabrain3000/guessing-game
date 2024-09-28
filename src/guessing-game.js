b57bff0f36e65ad4111f667c5fcbea67b653e307
gigabrain3000 <vladislav.ivanchikov15@gmail.com>
2024-09-28 20:52:49 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let candidate = Math.round((this.min + this.max) / 2);
+        this.candidate = candidate;
+        return this.candidate;
     }
 
     lower() {
-
+        this.max = this.candidate;
     }
 
     greater() {
-
+        this.min = this.candidate;
     }
 }
 
